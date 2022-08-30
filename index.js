8a5fb4f84a5157d270ef156e04137dcbed18f1ed
charlie reyes <charliereyes1228@gmail.com>
2022-08-30 03:02:33 -0700

    changed interface started map

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-//add map
+ //build map 
+  var map = L.map('map').setView([33.9547,-118.2120 ], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+     attribution: '© OpenStreetMap'
+  }).addTo(map);
+  var marker = L.marker([33.9460,-118.1852 ]).addTo(map);
+  marker.bindPopup("<b>South Gate Park !</b><br> You are here.").openPopup();
 
 
-
-
-
-//get user location
-
-
-
-
-//use forsquare for buisnesses
 
 
